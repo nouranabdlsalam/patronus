06b46b4c54bf327890e79a5ce95cc2f69c03df75
nouranabdlsalam <nouranabdlsalam@gmail.com>
2025-04-15 01:14:26 +0200

    added threat remediation and some navigation bars

--- app/src/main/java/com/example/patronus/MainActivity.java
+++ app/src/main/java/com/example/patronus/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.patronus;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.VpnService;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int VPN_REQUEST_CODE = 100;
 
     Button scan;
 
@@ -16,8 +21,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent prepareIntent = VpnService.prepare(this);
+        if (prepareIntent != null) {
+            startActivityForResult(prepareIntent, VPN_REQUEST_CODE);
+
+        } else {
+            Log.i("Main", "Requesting VPN permission...");
+            startService(new Intent(this, VPN.class));
+        }
+
         scan = findViewById(R.id.scan_test);
-        Intent i = new Intent(this, ScanScreenActivity.class);
-        startActivity(i);
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ScanScreenActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == VPN_REQUEST_CODE && resultCode == RESULT_OK) {
+            startService(new Intent(this, VPN.class));
+        } else {
+            Toast.makeText(this, "VPN permission denied", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/patronus/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.example.patronus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+    }
+
+}
